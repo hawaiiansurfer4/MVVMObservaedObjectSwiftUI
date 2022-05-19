fb9a4e0ed8a87b1c516a0601627d4b55909aacbb
Sean Murphy <sdmurphy.1999@gmail.com>
2022-05-19 15:57:51 -0400

    intial commit

---
+++ MVVMObservaedObjectSwiftUI/MVVMObservaedObjectSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMObservaedObjectSwiftUIApp.swift
+//  MVVMObservaedObjectSwiftUI
+//
+//  Created by Sean Murphy on 5/19/22.
+//
+
+import SwiftUI
+
+@main
+struct MVVMObservaedObjectSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
