307b5900223db4cce2e2eca3406cc2edaa1eaa3e
Sean Murphy <sdmurphy.1999@gmail.com>
2022-05-20 14:59:54 -0400

    Finish tutorial app, not a working api so will not fetch

--- MVVMObservaedObjectSwiftUI/ContentView.swift
+++ MVVMObservaedObjectSwiftUI/ContentView.swift
@@ -6,19 +6,75 @@
 //
 
 import SwiftUI
+import UIKit
 
 let apiUrl = "https://api.letsbuildthatapp.com/static/courses.json"
 
+struct Course: Identifiable, Codable {
+    var id = UUID()
+    var name: String
+
+    init(id: UUID, name: String) {
+        self.id = id
+        self.name = name
+    }
+}
+
+class CoursesViewModel: ObservableObject {
+    @Published var messages = "Messages inside observable oject"
+    @Published var courses = [Course]()
+    var coursesToShow: [String] = []
+    func changeMessage() {
+        self.messages = "Blah Blah Blah"
+    }
+
+    func fetchCourses() {
+        guard let url = URL(string: apiUrl) else { return }
+        let session = URLSession(configuration: .default)
+        let task = try session.dataTask(with: url) { data, resp, error in
+            if error != nil {
+                return
+            }
+            if let safeData = data {
+
+                self.parseJSON(safeData)
+            }
+        }.resume()
+    }
+
+    func parseJSON(_ courseData: Data) -> [Course]? {
+        let decoderr = JSONDecoder()
+
+        do {
+            let decodedCourses = try decoderr.decode(Course.self, from: courseData)
+            for i in decodedCourses.name {
+                coursesToShow.append(String(i))
+                courses.append(decodedCourses as Course)
+            }
+        } catch {
+            return nil
+        }
+        return courses
+    }
+}
+
 struct ContentView: View {
-    
+
+    @ObservedObject var coursesVM = CoursesViewModel()
+
     var body: some View {
         NavigationView {
             ScrollView {
-                Text("Hello Everyone from YOUTUBE")
+                Text(coursesVM.messages)
+
+                ForEach(coursesVM.courses) { course in
+                    Text(course.name)
+                }
             }.navigationBarTitle("Courses")
                 .navigationBarItems(trailing: Button(action: {
-                    print("Fetching json data")
-                }, label: {
+                print("Fetching json data")
+                self.coursesVM.fetchCourses()
+            }, label: {
                     Text("Fetch Courses")
                 }))
         }
